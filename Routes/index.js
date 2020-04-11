ca58744a9f2d267f77f99bf2961ff9840c2670e7
Francisco Dadone <dadonefrancisco@hotmail.com.ar>
2020-04-11 09:10:10 -0300

    added login page

--- Routes/index.js
+++ Routes/index.js
@@ -25,4 +25,43 @@ router.get('/register', (req, res) => {
     });
 });
 
+//Register handle
+router.post('/register', (req, res) => {
+    let errors = [];
+    const { username, email, password, password2 } = req.body;
+
+    //Check required fields
+    if(!email || !username || !password || !password2) {
+        errors.push({ msg: 'Please fill all fields.' });
+    }
+
+    // Check password match
+    if(password !== password2) {
+        errors.push({ msg: "Passwords don't match" });
+    }
+
+    //Check password lenght
+    if(password.lenght < 6) {
+        errors.push({ msg: 'Password should be at least 6 characters!' });
+    }
+
+
+    if(errors.length > 0) {
+        res.render('register', {
+            page: 'register',
+            title: 'Chatty - Registro',
+            errors,
+            username,
+            email
+        });
+        console.log("Register attempt ERRORS: ", errors);
+    } else {
+        res.send("Success");
+    }
+
+    
+});
+
+
+
 module.exports = router;
--- app.js
+++ app.js
@@ -2,21 +2,33 @@ const express = require('express');
 const path = require('path');
 const logger = require('./middlewares/logger');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Mongo database connected.'))
+    .catch(err => console.log(err));
+
 
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 // Init logger middleware
 app.use(logger);
 
+//Index route
 app.use('/', require('./Routes/index'));
-//app.use('/users', require('./Routes/users'));
 
 // Public folder
 app.use(express.static("public"));
